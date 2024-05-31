623243298f0f25ae9ed0e7b5e9b8c931283d6629
DilbaniEB <claudia.enriquez@globant.com>
2024-05-31 15:26:43 -0500

    Spring Initializr

---
+++ stellar-home/src/main/java/com/stellarHome/stellar/home/StellarHomeApplication.java
@@ -0,0 +1,14 @@
+package com.stellarHome.stellar.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarHomeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StellarHomeApplication.class, args);
+	}
+
+}
---
+++ stellar-home/src/test/java/com/stellarHome/stellar/home/StellarHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stellarHome.stellar.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
