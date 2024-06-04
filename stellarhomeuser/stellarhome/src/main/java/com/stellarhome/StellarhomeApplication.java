9661fe32787bd60d4a5903e5575c179fd4c037c5
DilbaniEB <claudia.enriquez@globant.com>
2024-06-04 15:14:43 -0500

    SpringBoot 17 and dependence

--- stellarhomeuser/stellarhome/src/main/java/com/stellarhome/StellarhomeApplication.java
+++ stellarhomeuser/stellarhome/src/main/java/com/stellarhome/StellarhomeApplication.java
@@ -1,14 +1,13 @@
-package com.stellarHome.stellar.home;
+package com.stellarhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StellarHomeApplication {
+public class StellarhomeApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(StellarHomeApplication.class, args);
+		SpringApplication.run(StellarhomeApplication.class, args);
 	}
 
 }
--- stellarhomeuser/stellarhome/src/test/java/com/stellarhome/StellarhomeApplicationTests.java
+++ stellarhomeuser/stellarhome/src/test/java/com/stellarhome/StellarhomeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.stellarHome.stellar.home;
+package com.stellarhome;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StellarHomeApplicationTests {
+class StellarhomeApplicationTests {
 
 	@Test
 	void contextLoads() {
