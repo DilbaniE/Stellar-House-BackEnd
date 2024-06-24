e836bd0cf1cc338f70c9d24fa39a5a8a9b9b23cc
DilbaniEB <claudia.enriquez@globant.com>
2024-06-24 10:59:25 -0500

    user repository 0012

--- stellarhome/src/main/java/com/stellarhome/controller/UserController.java
+++ stellarhome/src/main/java/com/stellarhome/controller/UserController.java
@@ -1,17 +1,5 @@
-package com.stellarhome.controller;
+package com.stellarhome.Controller;
 
-import com.stellarhome.endPoints.IUserEndPoint;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.PushBuilder;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping(IUserEndPoint.USER_BASE_URL)
-@Tag(name = "Stellar Home", description = "Points for managing products")
 public class UserController {
-
-
 
 }
--- stellarhome/src/main/java/com/stellarhome/model/UserEntity.java
+++ stellarhome/src/main/java/com/stellarhome/model/UserEntity.java
@@ -1,4 +1,32 @@
 package com.stellarhome.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@NotNull
+@Table(name = "users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name="name", nullable = false)
+    private String name;
+    @Column(name="last_name", nullable = false)
+    private String lastName;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @Column(name = "password", unique = true, nullable = false)
+    private String password;
+    @Column(name = "address", nullable = false)
+    private String address;
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
 }
