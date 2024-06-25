d8f9363e6236efe2b5c47706d1750ccd72dcdff8
DilbaniEB <claudia.enriquez@globant.com>
2024-06-24 19:55:28 -0500

    creating user 0013

--- stellarhome/src/main/java/com/stellarhome/service/UserService.java
+++ stellarhome/src/main/java/com/stellarhome/service/UserService.java
@@ -9,7 +9,33 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    
+    @Autowired
+    private IUserRepository iUserRepository;
+    @Transactional
+    public UserDTO createUser(UserDTO userDTO) {
+        // Convertir UserDTO a UserEntity
+        UserEntity userEntity = UserEntity.builder()
+                .name(userDTO.getName())
+                .lastName(userDTO.getLastName())
+                .email(userDTO.getEmail())
+                .password(userDTO.getPassword())
+                .address(userDTO.getAddress())
+                .phone(userDTO.getPhone())
+                .build();
+        // Guardar UserEntity en la base de datos
+        UserEntity savedEntity = iUserRepository.save(userEntity);
+        // Convertir UserEntity a UserDTO
+        UserDTO newUserDTO = UserDTO.builder()
+                .id(savedEntity.getId())
+                .name(savedEntity.getName())
+                .lastName(savedEntity.getLastName())
+                .email(savedEntity.getEmail())
+                .password(savedEntity.getPassword())
+                .address(savedEntity.getAddress())
+                .phone(savedEntity.getPhone())
+                .build();
+        return newUserDTO;
+    }
 
 
 }
