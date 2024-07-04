cd6164413bfb8fb64fbf073aff0a152b627c84b1
DilbaniEB <claudia.enriquez@globant.com>
2024-07-04 14:41:08 -0500

    URL base and URL methods controller

--- src/main/java/com/stellarhome/controller/UserController.java
+++ src/main/java/com/stellarhome/controller/UserController.java
@@ -1,4 +1,41 @@
 package com.stellarhome.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @PostMapping("/createUser")
+   public ResponseEntity<String> createUser(){
+       return null;
+   }
+
+    @GetMapping("/getUser")
+    public ResponseEntity<String> getUser(){
+        return null;
+    }
+
+    @PutMapping("/updateUser")
+    public ResponseEntity<String> updateUser(){
+        return null;
+    }
+
+    @PatchMapping("/patchUser")
+    public ResponseEntity<String> patchUser(){
+        return null;
+    }
+
+    @ DeleteMapping("/deleteUser")
+    public ResponseEntity<String> deleteUser(){
+        return null;
+    }
+
+
 }
