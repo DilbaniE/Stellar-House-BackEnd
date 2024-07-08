eb8653328adabf1436297d3647aaa65770b8ab6f
DilbaniEB <claudia.enriquez@globant.com>
2024-07-08 16:50:40 -0500

    Add methods to service and update metods controller

--- src/main/java/com/stellarhome/controller/UserController.java
+++ src/main/java/com/stellarhome/controller/UserController.java
@@ -1,40 +1,61 @@
 package com.stellarhome.controller;
 
+import com.stellarhome.model.UserEntity;
+import com.stellarhome.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    @PostMapping("/createUser")
-   public ResponseEntity<String> createUser(){
-       return null;
-   }
 
+    @Autowired
+    private UserService userService;
+    @PostMapping("/createUser")
+    public UserEntity createUser(@RequestBody UserEntity userEntity){
+        return userService.saveUser(userEntity);
+    }
     @GetMapping("/getUser")
-    public ResponseEntity<String> getUser(){
-        return null;
+    public UserEntity getUser(@PathVariable Integer id){
+        return userService.getById(id);
     }
 
+    @GetMapping("/getAll")
+    public List<UserEntity> getAllUsers(){
+        return userService.getAll();
+    }
     @PutMapping("/updateUser")
-    public ResponseEntity<String> updateUser(){
-        return null;
+    public ResponseEntity<UserEntity> updateUser(@PathVariable Integer id, @RequestBody UserEntity userEntity){
+        UserEntity userUpdate = userService.updateUser(id, userEntity);
+        if(userUpdate != null){
+            return ResponseEntity.ok(userUpdate);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @PatchMapping("/patchUser")
-    public ResponseEntity<String> patchUser(){
-        return null;
+    public ResponseEntity<UserEntity> patchUser(@PathVariable Integer id, @RequestBody Map<String, Object> fields){
+        UserEntity userUpdatePacth = userService.patchUser(id, fields);
+        if(userUpdatePacth != null){
+            return ResponseEntity.ok(userUpdatePacth);
+        }
+        return ResponseEntity.notFound().build();
     }
 
-    @ DeleteMapping("/deleteUser")
-    public ResponseEntity<String> deleteUser(){
-        return null;
+    @DeleteMapping("/deleteUser")
+    public void deleteUser(@PathVariable Integer id){
+        userService.deleteUser(id);
     }
 
 
--- src/main/java/com/stellarhome/service/UserService.java
+++ src/main/java/com/stellarhome/service/UserService.java
@@ -1,4 +1,73 @@
 package com.stellarhome.service;
 
+import com.stellarhome.model.UserEntity;
+import com.stellarhome.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+
+@Service
 public class UserService {
+
+    @Autowired
+    private IUserRepository iUserRepository;
+
+    public List<UserEntity> getAll(){
+        return iUserRepository.findAll();
+    }
+
+    public UserEntity getById(Integer id){
+        return iUserRepository.findById(id).orElse(null);
+    }
+
+    public UserEntity saveUser(UserEntity userEntity){
+        return iUserRepository.save(userEntity);
+    }
+
+    public UserEntity updateUser(Integer id, UserEntity userEntity){
+        if(iUserRepository.existsById(id)){
+            userEntity.setId(id);
+            return iUserRepository.save(userEntity);
+        }
+        return null;
+    }
+
+    public UserEntity patchUser(Integer id, Map<String, Object> fields){
+        Optional<UserEntity> optionalUser = iUserRepository.findById(id);
+        if(optionalUser.isPresent()){
+            UserEntity userEntity = optionalUser.get();
+            fields.forEach((key, value) ->{
+                switch (key){
+                    case "name":
+                        userEntity.setName((String) value);
+                        break;
+                    case "lastName":
+                        userEntity.setLastName((String) value);
+                        break;
+                    case "email":
+                        userEntity.setEmail((String) value);
+                        break;
+                    case "password":
+                        userEntity.setPassword((String) value);
+                        break;
+                    case "address":
+                        userEntity.setAddress((String) value);
+                        break;
+                    case "phone":
+                        userEntity.setPhone((String) value);
+                        break;
+                    default:
+                        break;
+                }
+            });
+            return iUserRepository.save(userEntity);
+        }
+        return null;
+    }
+    public void deleteUser(Integer id){
+        iUserRepository.deleteById(id);
+    }
 }
